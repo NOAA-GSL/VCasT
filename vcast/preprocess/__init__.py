c99a3be4fd5edbdf7fb589529c8aee59c0f337a6
VanderleiVargas-NOAA <vanderlei.vargas@noaa.gov>
2025-05-13 21:43:36 +0000

    Added import checks

--- vcast/preprocess/__init__.py
+++ vcast/preprocess/__init__.py
@@ -1,21 +1,26 @@
-# preprocess/__init__.py
-
 """
-VCasT I/O Module
-----------------
-This module handles file operations, and preprocessing.
+VCasT Preprocess Module
+-----------------------
+This module handles file operations and preprocessing.
 
 Modules:
 - file_checker.py: Checks file formats and ensures compatibility.
 - preprocess.py: Handles data preprocessing and formatting.
 
 Available Classes:
-- FileChecker: Identifies file formats and validates input data.
+- FileChecker: Identifies file formats and validates input data (requires pygrib).
 - Preprocessor: Processes input files and prepares them for analysis.
 """
 
-from .file_checker import FileChecker
-from .preprocess import Preprocessor
+try:
+    from .file_checker import FileChecker
+except ImportError:
+    FileChecker = None
+
+try:
+    from .preprocess import Preprocessor
+except ImportError:
+    Preprocessor = None
 
 __all__ = [
     "FileChecker",
